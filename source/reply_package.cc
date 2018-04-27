c43368dff1957de7cf89f29c65146a428a84a457
Ben LaRoque <benjamin.laroque@gmail.com>
2018-04-27 15:50:27 -0700

    adding payload to reply_info

--- source/reply_package.cc
+++ source/reply_package.cc
@@ -16,10 +16,11 @@ namespace dripline
 {
     LOGGER( dlog, "reply_package" );
 
-    reply_info::reply_info( bool a_send_result, retcode_t a_return_code, const std::string& a_return_msg ) :
+    reply_info::reply_info( bool a_send_result, retcode_t a_return_code, const std::string& a_return_msg, const scarab::param_node a_payload ) :
             f_send_result( a_send_result ),
             f_return_code( a_return_code ),
-            f_return_msg( a_return_msg )
+            f_return_msg( a_return_msg ),
+            f_payload( a_payload )
     {}
 
     reply_package::reply_package( const service* a_service, request_ptr_t a_request ) :
