218248af7e1baf8e71155611aaba8ec88889fb6e
Noah Oblath <nsoblath@gmail.com>
2018-08-25 14:48:32 -0700

    Convert dripline_error to use strings instead of stringstream

--- library/dripline_error.cc
+++ library/dripline_error.cc
@@ -8,14 +8,14 @@ namespace dripline
 
     dripline_error::dripline_error() :
             ::std::exception(),
-            f_error( "" ),
+            f_error(),
             f_retcode( retcode_t::success )
     {
     }
 
     dripline_error::dripline_error( const dripline_error& an_error ) :
             std::exception(),
-            f_error( an_error.f_error.str() ),
+            f_error( an_error.f_error ),
             f_retcode( an_error.f_retcode )
     {
     }
@@ -26,7 +26,7 @@ namespace dripline
 
     const char* dripline_error::what() const throw ()
     {
-        return f_error.str().c_str();
+        return f_error.c_str();
     }
 
 }
