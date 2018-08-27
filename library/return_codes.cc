b74800bc85568bb443e2473a71f0e3ebcd9d888e
Noah Oblath <nsoblath@gmail.com>
2018-08-26 17:00:59 -0700

    Added the other return codes

--- library/return_codes.cc
+++ library/return_codes.cc
@@ -11,6 +11,36 @@ namespace dripline
 {
     IMPL_DL_RET_CODE( success, 0 );
 
+    IMPL_DL_RET_CODE( warning_no_action_taken, 1 );
+
+    IMPL_DL_RET_CODE( amqp_error, 100 );
+    IMPL_DL_RET_CODE( amqp_error_broker_connection, 101 );
+    IMPL_DL_RET_CODE( amqp_error_routingkey_notfound, 102 );
+
+    IMPL_DL_RET_CODE( device_error, 200 );
+    IMPL_DL_RET_CODE( device_error_connection, 201 );
+    IMPL_DL_RET_CODE( device_error_no_resp, 202 );
+
+    IMPL_DL_RET_CODE( message_error, 300 );
+    IMPL_DL_RET_CODE( message_error_no_encoding, 301 );
+    IMPL_DL_RET_CODE( message_error_decoding_fail, 302 );
+    IMPL_DL_RET_CODE( message_error_bad_payload, 303 );
+    IMPL_DL_RET_CODE( message_error_invalid_value, 304 );
+    IMPL_DL_RET_CODE( message_error_timeout, 305 );
+    IMPL_DL_RET_CODE( message_error_invalid_method, 306 );
+    IMPL_DL_RET_CODE( message_error_access_denied, 307 );
+    IMPL_DL_RET_CODE( message_error_invalid_key, 308 );
+    IMPL_DL_RET_CODE( message_error_dripline_deprecated, 309 );
+
+    IMPL_DL_RET_CODE( database_error, 400 );
+
+    IMPL_DL_RET_CODE( daq_error, 500 );
+    IMPL_DL_RET_CODE( daq_not_enabled, 501 );
+    IMPL_DL_RET_CODE( daq_running, 502 );
+
+    IMPL_DL_RET_CODE( unhandled_exception, 999 );
+
+
     return_code::return_code() :
             f_message()
     {
--- library/return_codes.hh
+++ library/return_codes.hh
@@ -38,18 +38,48 @@ namespace dripline
     };
 
 #define DEFINE_DL_RET_CODE( name ) \
-    class DRIPLINE_API return_##name : public return_code \
+    class DRIPLINE_API dl_##name : public return_code \
     { \
         public: \
-            return_##name() : return_code() {} \
-            virtual ~return_##name() {} \
+            dl_##name() : return_code() {} \
+            virtual ~dl_##name() {} \
             static const unsigned f_code; \
     };
     
 #define IMPL_DL_RET_CODE( name, the_code ) \
-    const unsigned return_##name::f_code = the_code;
+    const unsigned dl_##name::f_code = the_code;
     
     DEFINE_DL_RET_CODE( success );
+
+    DEFINE_DL_RET_CODE( warning_no_action_taken );
+
+    DEFINE_DL_RET_CODE( amqp_error );
+    DEFINE_DL_RET_CODE( amqp_error_broker_connection );
+    DEFINE_DL_RET_CODE( amqp_error_routingkey_notfound );
+
+    DEFINE_DL_RET_CODE( device_error );
+    DEFINE_DL_RET_CODE( device_error_connection );
+    DEFINE_DL_RET_CODE( device_error_no_resp );
+
+    DEFINE_DL_RET_CODE( message_error );
+    DEFINE_DL_RET_CODE( message_error_no_encoding );
+    DEFINE_DL_RET_CODE( message_error_decoding_fail );
+    DEFINE_DL_RET_CODE( message_error_bad_payload );
+    DEFINE_DL_RET_CODE( message_error_invalid_value );
+    DEFINE_DL_RET_CODE( message_error_timeout );
+    DEFINE_DL_RET_CODE( message_error_invalid_method );
+    DEFINE_DL_RET_CODE( message_error_access_denied );
+    DEFINE_DL_RET_CODE( message_error_invalid_key );
+    DEFINE_DL_RET_CODE( message_error_dripline_deprecated );
+
+    DEFINE_DL_RET_CODE( database_error );
+
+    DEFINE_DL_RET_CODE( daq_error );
+    DEFINE_DL_RET_CODE( daq_not_enabled );
+    DEFINE_DL_RET_CODE( daq_running );
+
+    DEFINE_DL_RET_CODE( unhandled_exception );
+
 
     template< class x_streamable >
     return_code& return_code::operator<<( x_streamable a_fragment )
