7f0c49a3263264adf794b21ce21c151fd48b62bc
Noah Oblath <nsoblath@gmail.com>
2019-01-31 11:36:06 -0800

    Added a footer help message to talk about values and keyword arguments

--- executables/dl_agent.cc
+++ executables/dl_agent.cc
@@ -32,6 +32,14 @@ int main( int argc, char** argv )
     the_main.require_subcommand(1); // we use subcommands here
     the_main.fallthrough(); // pass any options not handled by the subcommands to the main app (this)
 
+    the_main.footer( std::string() + "\nValues and Keyword Arguments:\n\n" +
+            "  Further positional arguments (other than the routing key and specifier)\n" +
+            "  can be used to add to the values array in the payload and to other parts of the payload.\n\n" +
+            "  Arguments in the form [key]=[value] will be assumed to be keyword arguments.\n" +
+            "  Other arguments will be assumed to be entries in the values array.\n\n" +
+            "  The key portion of a keyword argument is an address that can specify\n" +
+            "  both node and array locations (e.g. my.value.0)." );
+
     // Routing key
     the_main.add_config_option< std::string >( "routing_key", "rk", "Set the routing key" )->required();
 
