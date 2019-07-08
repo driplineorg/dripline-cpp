02442ef23c41e453279d730f2a05e42a091c8d0d
Noah Oblath <nsoblath@gmail.com>
2019-07-08 10:50:51 -0700

    Complete the set of default CL options for dripline configuration values

--- library/dripline_config.cc
+++ library/dripline_config.cc
@@ -57,7 +57,14 @@ namespace dripline
         an_app.add_config_option< std::string >( "-b,--broker", "dripline.broker", "Set the dripline broker address" );
         an_app.add_config_option< unsigned >( "-p,--port", "dripline.broker-port", "Set the port for communication with the dripline broker" );
         an_app.add_config_option< std::string >( "--auth-file", "dripline.auth-file", "Set the authentication file path" );
+        an_app.add_config_option< std::string >( "--requests-exchange", "dripline.requests-exchange", "Set the name of the requests exchange" );
+        an_app.add_config_option< std::string >( "--alerts-exchange", "dripline.alerts-exchange", "Set the name of the alerts exchange" );
         an_app.add_config_option< unsigned >( "--max-payload", "dripline.max-payload-size", "Set the maximum payload size (in bytes)" );
+        an_app.add_config_option< unsigned >( "--loop-timeout-ms" "dripline.loop-timeout-ms", "Set the timeout for thread loops in ms" );
+        an_app.add_config_option< unsigned >( "--message-wait-ms" "dripline.message-wait-ms", "Set the time to wait for a full multi-part message in ms" );
+        an_app.add_config_option< std::string >( "--heartbeat-routing-key", "dripline.heartbeat-routing-key", "Set the first token of heartbeat routing keys: [token].[origin]" );
+        an_app.add_config_option< unsigned >( "--heartbeat-interval-s", "dripline.heartbeat-interval-s", "Set the interval between heartbeats in s" );
+
         return;
     }
 
