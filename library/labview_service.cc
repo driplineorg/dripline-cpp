ccf51d6c111f51d9013e9f9e2900b6798d68ce5f
Noah Oblath <nsoblath@gmail.com>
2019-08-20 16:17:17 -0700

    Added labview option to the main CMakeLists file, and added labview_service class.

---
+++ library/labview_service.cc
@@ -0,0 +1,57 @@
+/*
+ * labview_service.cc
+ *
+ *  Created on: Aug 20, 2019
+ *      Author: N.S. Oblath
+ */
+
+#include "labview_service.hh"
+
+#include "param.hh"
+#include "param_json.hh"
+
+
+namespace dripline
+{
+    std::string labview_sys_interface::perform_get( const std::string& a_thing )
+    {
+        scarab::param_node t_node;
+        t_node.add( "value", 5 );
+        std::string t_node_string;
+        scarab::param_output_json t_codec;
+        t_codec.write_string( t_node, t_node_string );
+        return t_node_string;
+    }
+
+
+    labview_service::labview_service( const scarab::param_node& a_config ) :
+            service( a_config )
+    {
+    }
+
+    labview_service::~labview_service()
+    {
+    }
+
+    reply_ptr_t labview_service::do_get_request( const request_ptr_t a_request )
+    {
+        std::string t_reply_payload_string = labview_sys_interface::perform_get( a_request->parsed_specifier().unparsed() );
+        scarab::param_input_json t_codec;
+        scarab::param_ptr_t t_payload_ptr = t_codec.read_string( t_reply_payload_string );
+        reply_ptr_t t_reply = a_request->reply( dl_success(), "Get operation succeeded", std::move(t_payload_ptr) );
+        return t_reply;
+    }
+
+    /*
+    reply_ptr_t do_set_request( const request_ptr_t a_request )
+    {
+
+    }
+
+    reply_ptr_t do_cmd_request( const request_ptr_t a_request )
+    {
+
+    }
+*/
+
+} /* namespace dripline */
---
+++ library/labview_service.hh
@@ -0,0 +1,47 @@
+/*
+ * labview_service.hh
+ *
+ *  Created on: Aug 20, 2019
+ *      Author: N.S. Oblath
+ */
+
+#ifndef LIBRARY_LABVIEW_SERVICE_HH_
+#define LIBRARY_LABVIEW_SERVICE_HH_
+
+#include "service.hh"
+
+namespace dripline
+{
+    class labview_sys_interface
+    {
+        protected:
+            labview_sys_interface() {}
+            ~labview_sys_interface() {}
+
+        public:
+            // get interface:
+            //    parameters:
+            //      - a_thing: specifies what is being retrieved
+            //    return: JSON-formatted string with the returned information
+            static std::string perform_get( const std::string& a_thing );
+    };
+
+    class labview_service : public service
+    {
+        public:
+            labview_service( const scarab::param_node& a_config = scarab::param_node() );
+            virtual ~labview_service();
+
+        protected:
+            // uses labview_sys_interface::perform_get
+            // submits unparsed specifier as the `a_thing` argument
+            // takes returned JSON string, converts to param, and adds as the payload of the reply
+            virtual reply_ptr_t do_get_request( const request_ptr_t a_request );
+            //virtual reply_ptr_t do_set_request( const request_ptr_t a_request );
+            //virtual reply_ptr_t do_cmd_request( const request_ptr_t a_request );
+
+    };
+
+} /* namespace dripline */
+
+#endif /* LIBRARY_LABVIEW_SERVICE_HH_ */
