397feceb26db2eb01c0986d0f01319750dbeef2f
Noah Oblath <nsoblath@gmail.com>
2019-10-10 10:51:14 -0700

    Added a second test for throwing throw_replies with payloads

--- testing/test_throw_reply.cc
+++ testing/test_throw_reply.cc
@@ -7,19 +7,48 @@
 
 #include "dripline_error.hh"
 
+#include "logger.hh"
+
 #include "catch.hpp"
 
+LOGGER( testlog, "test_throw_reply" );
 
 TEST_CASE( "throw_reply", "[error]" )
 {
+    // simple throw and manual return-code set
     try
     {
-        throw dripline::throw_reply( dripline::dl_success() ) ;
+        throw dripline::throw_reply( dripline::dl_success() );
     }
     catch( dripline::throw_reply& e )
     {
         REQUIRE( e.ret_code().rc_value() == dripline::dl_success::s_value );
         e.set_return_code( dripline::dl_amqp_error() );
         REQUIRE( e.ret_code().rc_value() == dripline::dl_amqp_error::s_value );
+    }
+
+    // reply with message and payload
+    try
+    {
+        dripline::throw_reply t_throw_reply( dripline::dl_success{} );
+        scarab::param_ptr_t t_payload( new scarab::param_value( 5 ) );
+        t_throw_reply.set_payload( std::move( t_payload ) );
+        throw t_throw_reply << "Hello";
+    }
+    catch( dripline::throw_reply& e )
+    {
+        REQUIRE( e.ret_code().rc_value() == dripline::dl_success::s_value );
+        REQUIRE( std::string(e.what()) == std::string("Return code: success (0) -- Hello") );
+        REQUIRE( e.payload().as_value().as_uint() == 5 );
+
+        // setting the payload with operator=, as you might if you had a reply object you were setting the payload into
+        dripline::throw_reply t_payload_from;
+        t_payload_from.set_payload( scarab::param_ptr_t(new scarab::param_value( 10 )) );
+        e.payload() = t_payload_from.payload();
+        REQUIRE( e.payload().as_value().as_uint() == 10 );
+    }
+    catch( std::exception& e )
+    {
+        LERROR( testlog, "Caught standard exception: " << e.what() );
     }
 }
