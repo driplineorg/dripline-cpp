188c13f799016ea9cca6e896b696c3022facb6c8
Noah Oblath <nsoblath@gmail.com>
2019-10-10 11:37:22 -0700

    Reorganized test_throw_reply a little

--- testing/test_throw_reply.cc
+++ testing/test_throw_reply.cc
@@ -33,6 +33,14 @@ TEST_CASE( "throw_reply", "[error]" )
         dripline::throw_reply t_throw_reply( dripline::dl_success{} );
         scarab::param_ptr_t t_payload( new scarab::param_value( 5 ) );
         t_throw_reply.set_payload( std::move( t_payload ) );
+        REQUIRE( t_throw_reply.payload().as_value().as_uint() == 5 );
+
+        // setting the payload with operator=, as you might if you had a reply object you were setting the payload into
+        dripline::throw_reply t_payload_from;
+        t_payload_from.set_payload( scarab::param_ptr_t(new scarab::param_value( 10 )) );
+        t_throw_reply.payload() = std::move(t_payload_from.payload());
+        REQUIRE( t_throw_reply.payload().as_value().as_uint() == 10 );
+
         throw t_throw_reply << "Hello";
     }
     catch( dripline::throw_reply& e )
@@ -41,14 +49,10 @@ TEST_CASE( "throw_reply", "[error]" )
         REQUIRE( std::string(e.what()) == std::string("Return code: success (0) -- Hello") );
         REQUIRE( e.payload().as_value().as_uint() == 5 );
 
-        // setting the payload with operator=, as you might if you had a reply object you were setting the payload into
-        dripline::throw_reply t_payload_from;
-        t_payload_from.set_payload( scarab::param_ptr_t(new scarab::param_value( 10 )) );
-        e.payload() = t_payload_from.payload();
-        REQUIRE( e.payload().as_value().as_uint() == 10 );
     }
     catch( std::exception& e )
     {
         LERROR( testlog, "Caught standard exception: " << e.what() );
+        REQUIRE( false ); // we should never get here
     }
 }
