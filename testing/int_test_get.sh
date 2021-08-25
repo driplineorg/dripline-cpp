1632cadc28064e6d7400ecb53c9bdb5a93effbff
Noah Oblath <noah.oblath@pnnl.gov>
2021-08-25 15:29:34 -0700

    Attempt to switch to using bash_test_tools for bash-based testing

--- testing/int_test_get.sh
+++ testing/int_test_get.sh
@@ -1,10 +1,21 @@
 #! /bin/bash
 
-# wait to make sure service connects
-sleep 2s
+source /root/bash_test_tools
 
-dl-agent -vv -b rabbit_broker --auth-file /root/authentication.json get simple
+function setup
+{
+    # wait to make sure service connects
+    sleep 2s
+}
 
-# TODO: make a better check of the return
 
-exit 0
+function test_get
+{
+    dl-agent -vv -b rabbit_broker --auth-file /root/authentication.json get simple
+
+    assert_terminated_normally
+    assert_exit_success
+    assert_output_contains "Return Message: Congrats, you performed an OP_GET"
+}
+
+testrunner
