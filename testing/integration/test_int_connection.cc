62d02349e2cb2fbda31cf72ae5eb2c9ab4da91ca
Noah Oblath <noah.oblath@pnnl.gov>
2021-09-02 13:53:10 -0700

    Update channel integration test code

--- testing/integration/test_int_connection.cc
+++ testing/integration/test_int_connection.cc
@@ -24,16 +24,73 @@ namespace dripline_test
             {
                 return core::open_channel();
             }
+
+            static bool setup_exchange( dripline::amqp_channel_ptr a_channel, const std::string& a_exchange )
+            {
+                return core::setup_exchange( a_channel, a_exchange );
+            }
+
+            static bool setup_queue( dripline::amqp_channel_ptr a_channel, const std::string& a_queue_name )
+            {
+                return core::setup_queue( a_channel, a_queue_name );
+            }
+
+            static bool bind_key( dripline::amqp_channel_ptr a_channel, const std::string& a_exchange, const std::string& a_queue_name, const std::string& a_routing_key )
+            {
+                return core::bind_key( a_channel, a_exchange, a_queue_name, a_routing_key );
+            }
+
+            static std::string start_consuming( dripline::amqp_channel_ptr a_channel, const std::string& a_queue_name )
+            {
+                return core::start_consuming( a_channel, a_queue_name );
+            }
+
+            static bool stop_consuming( dripline::amqp_channel_ptr a_channel, std::string& a_consumer_tag )
+            {
+                return core::stop_consuming( a_channel, a_consumer_tag );
+            }
+
+            static bool remove_queue( dripline::amqp_channel_ptr a_channel, const std::string& a_queue_name )
+            {
+                return core::remove_queue( a_channel, a_queue_name );
+            }
+
     };
 }
 
-TEST_CASE( "connection" )
+TEST_CASE( "channel" )
 {
-    dripline_test::test_core t_core( scarab::param_node(), "rabbit_broker", 0, "/root/authentication.json" );
+    using dripline_test::test_core;
+
+    test_core t_core( scarab::param_node(), "rabbit_broker", 0, "/root/authentication.json" );
 
     dripline::amqp_channel_ptr t_channel = t_core.wrap_open_channel();
 
     REQUIRE( t_channel );
+
+    REQUIRE( test_core::setup_exchange( t_channel, "requests" ) );
+    std::cerr << ">>>> pausing for 30 second" << std::endl;
+    std::this_thread::sleep_for( std::chrono::seconds(10) );
+    // for some reason i get an error when i run this command, though everything seems to be working correctly
+    //REQUIRE( t_channel->CheckExchangeExists( "requests" ) );
+
+    REQUIRE( test_core::setup_queue( t_channel, "test_queue" ) );
+    std::cerr << ">>>> pausing for 60 second" << std::endl;
+    std::this_thread::sleep_for( std::chrono::seconds(10) );
+    // for some reason i get an error when i run this command, though everything seems to be working correctly
+    //REQUIRE( t_channel->CheckQueueExists( "test_queue" ) );
+
+    REQUIRE( test_core::bind_key( t_channel, "requests", "test_queue", "test_key" ) );
+
+    std::string t_consumer_tag( test_core::start_consuming( t_channel, "test_queue" ) );
+    REQUIRE_FALSE( t_consumer_tag.empty() );
+
+    REQUIRE( test_core::stop_consuming( t_channel, t_consumer_tag ) );
+
+    REQUIRE( test_core::remove_queue( t_channel, "test_queue" ) );
+    std::this_thread::sleep_for( std::chrono::seconds(10) );
+    // for some reason i get an error when i run this command, though everything seems to be working correctly
+    //REQUIRE_FALSE( t_channel->CheckQueueExists( "test_queue" ) );
 }
 
 
