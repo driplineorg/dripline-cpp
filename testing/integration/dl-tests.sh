4cba9e262feda363c20b9226afde8a5f2514db24
Noah Oblath <nsoblath@gmail.com>
2024-05-28 17:20:16 -0700

    Specify the broker

--- testing/integration/dl-tests.sh
+++ testing/integration/dl-tests.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bats
 
 @test "ping simple" {
-    dl-agent -vv --auth-file /auths.json cmd simple -s ping
+    dl-agent -vv -b rabbit-broker --auth-file /auths.json cmd simple -s ping
 }
 
 @test "get simple" {
-    dl-agent -vv --auth-file /auths.json get simple
+    dl-agent -vv -b rabbit-broker --auth-file /auths.json get simple
 }
 
 @test "set simple" {
-    dl-agent -vv --auth-file /auths.json set simple 500
+    dl-agent -vv -b rabbit-broker --auth-file /auths.json set simple 500
 }
