fe92a3258fdd48661a0b591aa7e13159f0bf9ea0
Noah Oblath <nsoblath@gmail.com>
2024-06-13 17:22:48 -0700

    Add testing of CL and file overrides of username and password

--- testing/integration/dl-tests.sh
+++ testing/integration/dl-tests.sh
@@ -11,3 +11,11 @@
 @test "set simple" {
     dl-agent -vv -b rabbit-broker --auth-file /auths.json set simple 500
 }
+
+@test "user/pass on CL" {
+    dl-agent -vv -b rabbit-broker -u dripline --password dripline get simple
+}
+
+@test "user on CL/pass in file" {
+    dl-agent -vvv -b rabbit-broker -u dripline --password-file /root/password.txt get simple
+}
